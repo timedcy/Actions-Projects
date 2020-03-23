344523fa828df2c31b2d8260a492f9465ce63530
张华粮 <zhanghualiang18@mails.ucas.ac.cn>
2020-03-23 23:54:23 +0800

    FreeAirport.yml changes

--- FreeAirport/main.py
+++ FreeAirport/main.py
@@ -1,14 +1,14 @@
 import datetime
 
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import FileSystemLoader,Environment
 
 from providers import test
 
 
 def generate_md(source, date, ssr_link):
-    env = Environment(loader=FileSystemLoader("templates"))
-    template = env.get_template("base.md")
-    with open("README.md", "w+", encoding="utf-8") as fout:
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('base.md')
+    with open('README.md', 'w+', encoding='utf-8') as fout:
         md_content = template.render(source=source, date=date, ssr_link=ssr_link)
         fout.write(md_content)
 
