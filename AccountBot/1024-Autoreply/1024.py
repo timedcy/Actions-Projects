6cf2679f3dc4abab2deeba349c72d3af5f905875
张华粮 <zhanghualiang18@mails.ucas.ac.cn>
2020-09-06 20:49:57 +0800

    Signed-off-by: 张华粮 <zhanghualiang18@mails.ucas.ac.cn>

---
+++ AccountBot/1024-Autoreply/1024.py
@@ -0,0 +1,285 @@
+import requests
+import re
+import random
+import onetimepass as otp
+from time import sleep
+from urllib import parse
+import os
+from getver import Getver
+import config
+import logging
+
+class Autoreply:
+    result=None
+    over=False
+    flag=False
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+    loginurl = 'http://t66y.com/login.php'
+    url='http://t66y.com/thread0806.php?fid=7&search=today'
+    posturl='http://t66y.com/post.php?'
+    indexurl='http://t66y.com/index.php'
+    black_list=['htm_data/2003/7/3832698.html','htm_data/1602/7/37458.html','htm_data/1502/7/1331010.html','htm_data/2005/7/2520305.html','htm_data/2005/7/2404767.html']
+    s=requests.Session()
+    headers={
+        'Host': 't66y.com',
+        'Proxy-Connection': 'keep-alive',
+        'Referer': 'http://t66y.com/index.php',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4209.2 Safari/537.36'
+    }
+    headers1={
+        'Host': 't66y.com',
+        'Proxy-Connection': 'keep-alive',
+        'Referer': 'http://t66y.com/login.php',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4209.2 Safari/537.36'
+    }
+    headers2={
+        'Host': 't66y.com',
+        'Origin': 'null',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Proxy-Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4209.2 Safari/537.36'
+        }
+
+    def __init__(self,user,password,secret):
+        self.user= user.encode('gb2312')
+        self.password= password
+        self.secret =secret
+
+    def login1(self):
+        sleep(2)
+        Err=None
+        data={
+                'pwuser': self.user,
+                'pwpwd':  self.password,
+                'hideid': '0',
+                'cktime': '0',
+                'forward': 'http://t66y.com/post.php?',
+                'jumpurl': 'http://t66y.com/post.php?',
+                'step': '2'
+        }
+        login=self.s.post(self.loginurl,headers=self.headers,data=data)
+        login=login.text.encode('iso-8859-1').decode('gbk')
+        if login.find('登录尝试次数过多')!=-1:
+            Err='登录尝试次数过多,需输入验证码'
+            return Err
+        elif login.find('賬號已開啟兩步驗證')!=-1:
+            Err='賬號已開啟兩步驗證'
+            return Err
+
+    def login2(self):
+        sleep(2)
+        my_token = otp.get_totp(self.secret)
+        data={
+        'step': '2',
+        'cktime': '0',
+        'oneCode': str(my_token)
+        }
+        login=self.s.post(self.loginurl,headers=self.headers,data=data)
+        self.cookies=login.cookies
+        login=login.text.encode('iso-8859-1').decode('gbk')
+        if login.find('您已經順利登錄')!=-1:
+            res='已經順利登錄'
+            self.s.close()
+            return res
+
+    def getverwebp(self):
+        code=random.uniform(0,1)
+        code=round(code,16)
+        vercodeurl='http://t66y.com/require/codeimg.php?'+str(code)
+        image=self.s.get(vercodeurl,headers=self.headers1)
+        f=open('image.webp','wb')
+        f.write(image.content)
+        f.close()
+
+    def inputvercode(self,vercode):
+        sleep(2)
+        vercode=vercode
+        data={
+            'validate': vercode
+        }
+        login=self.s.post(self.loginurl,data=data,headers=self.headers1)
+        login=login.text.encode('iso-8859-1').decode('gbk')
+        if login.find('驗證碼不正確')!=-1:
+            Err='验证码不正确，请重新输入'
+            return Err
+
+    def gettodaylist(self):
+        pat=('htm_data/\w+/\w+/\w+.html')
+        con=self.s.get(self.url,headers=self.headers)
+        con = con.text.encode('iso-8859-1').decode('gbk')
+        match=re.findall(pat,con)
+        self.match=match
+        qiuzhutie=con.find('求片求助貼')
+        qiuzhutie=con[qiuzhutie-100:qiuzhutie]
+        if re.findall(pat,qiuzhutie)!=[]:
+            qiuzhutielink=re.findall(pat,qiuzhutie)
+        else:
+            qiuzhutielink=['no']
+            self.match.append('no')
+        self.logger.debug('求助帖链接是:'+qiuzhutielink[0])
+        self.black_list.append(qiuzhutielink[0])
+        try:
+            for data in self.black_list:
+                self.match.remove(data)
+        except:
+            print('移除失败，知道因为啥。。。')
+            pass
+
+    def getonelink(self):
+        geturl=''
+        m=random.randint(0,len(self.match)-1)
+        geturl='http://t66y.com/'+self.match[m]
+        self.geturl=geturl
+        tid=self.match[m][16:len(self.match[m])-5]
+        self.tid=tid
+        self.match.remove(self.match[m])
+        #print('请求链接是: '+geturl)
+
+    def browse(self):
+        res=requests.get(url=self.geturl,headers=self.headers,cookies=self.cookies)
+        #res=res.text.encode('iso-8859-1').decode('gbk')
+        #print(res)
+
+    #不知道啥用，留着吧
+    def getmatch(self):
+        sleep(2)
+        get=requests.get(self.geturl,headers=self.headers,cookies=self.cookies)
+        sleep(2)
+        get=get.text.encode('iso-8859-1').decode('gbk')
+        pat='<h4>.*</h4>'
+        res=re.search(pat,get)
+        res=res.group(0).replace('<h4>','').replace('</h4>','')
+        res='Re:'+res
+        self.res=res
+        #print(res)
+
+    def getreply(self):
+        #自定义回复内容，记得修改随机数
+        reply=['感谢分享','感谢你的分享','谢谢分享','多谢分享']
+        reply_m=random.randint(0,4)
+        reply_news=reply[reply_m]
+        self.reply_news=reply_news.encode('gb2312')
+        self.logger.debug("本次回复内容是:"+reply_news)
+
+    #暂时没用，看以后了
+    # def encodepost(self):
+    #     res=self.res.encode('gbk')
+    #     res=parse.quote(res)
+    #     self.encoderesult=res
+    #     print(self.encoderesult)
+    #     reply_news=self.reply_news.encode('gbk')
+    #     reply_news=parse.quote(reply_news)
+    #     self.encoderesult=res
+    #     self.encodereply=reply_news
+    #     #print(self.encodereply)
+
+    def postreply(self):
+        data={
+            'atc_usesign':'1',
+            'atc_convert':'1',
+            'atc_autourl': '1',
+            'atc_title': self.res ,
+            'atc_content': self.reply_news ,
+            'step': '2',
+            'action': 'reply',
+            'fid': '7',
+            'tid': self.tid ,
+            'atc_attachment': 'none',
+            'pid':'',
+            'article':'',
+            'touid':'',
+            'verify':'verify'
+        }
+        post=requests.post(self.posturl,data=data,headers=self.headers2,cookies=self.cookies)
+        post = post.text.encode('iso-8859-1').decode('gbk')
+        if post.find('發貼完畢點擊')!=-1:
+            status='回复成功'
+            return status
+        if post.find('所屬的用戶組')!=-1:
+            status='今日已达上限'
+            return status
+
+    def getnumber(self):
+        sleep(2)
+        index=requests.get(self.indexurl,headers=self.headers,cookies=self.cookies)
+        index = index.text.encode('iso-8859-1').decode('gbk')
+        pat='共發表帖子: \d{1,5}'
+        num=re.search(pat,index).group(0)
+        num=num.replace('共發表帖子: ','')
+        return num
+    
+    def debug(self,content):
+        self.logger.debug(content)
+
+if __name__ == "__main__":
+    n=0
+    success=None
+    suc=False
+    config.set_environ()
+    user= os.environ["USER"]
+    password= os.environ["PASSWORD"]
+    secret =os.environ["SECRET"]
+    auto=Autoreply(user,password,secret)
+
+    while success is None:
+        au=auto.login1()
+        if au=='登录尝试次数过多,需输入验证码':
+            auto.debug('登录尝试次数过多,需输入验证码')
+            auto.getverwebp()
+            getcd=Getver()
+            vercode=getcd.getcode()
+            auto.debug(vercode)
+            while auto.inputvercode(vercode)=='验证码不正确，请重新输入':
+                auto.debug('验证码不正确，请重新输入')
+                auto.getverwebp()
+                vercode=getcd.getcode()
+                auto.debug(vercode)
+            if auto.login1()=='賬號已開啟兩步驗證':
+                if auto.login2()=='已經順利登錄':
+                    auto.debug('登录成功')
+                    success = True
+                    au=''
+        else:
+            if au=='賬號已開啟兩步驗證':
+                if auto.login2()=='已經順利登錄':
+                    auto.debug('登录成功')
+                    success = True
+                    au=''
+    m=auto.getnumber()
+    auto.gettodaylist()
+    #回复
+    while n<10 and suc is False:
+        try:
+            auto.debug("当前在第"+str(n+1)+'个。')
+            auto.getonelink()
+            auto.browse()
+            auto.getreply()
+            auto.getmatch()
+            sleeptime=random.randint(1024,2048)
+            au=auto.postreply()
+            if au=='回复成功':
+                auto.debug('回复成功')
+                n=n+1
+                auto.debug('休眠'+str(sleeptime)+'s...')
+                sleep(sleeptime)
+                auto.debug('休眠完成')
+            elif au=='今日已达上限':
+                auto.debug('回复失败，今日次数已达10次')
+                suc=True
+            else:
+                auto.debug('1024限制！！！')
+                auto.debug('休眠'+str(sleeptime)+'s...')
+                sleep(sleeptime)
+                auto.debug('休眠完成')
+        except:
+            print('回复失败，重试')
+    n=auto.getnumber()
+    auto.debug('开始时发表帖子:'+m)
+    auto.debug('结束时发表帖子:'+n)
+    auto.debug('回复'+str(int(n)-int(m))+'次')
---
+++ AccountBot/1024-Autoreply/README.md
@@ -0,0 +1,70 @@
+# 1024-Autoreply
+
+因为考研在即，加上1024的回复机制确实有点繁琐，所以打算用个python脚本做一个自动回帖，本脚本主要适用于新手上路到侠客之间 
+
+<h4>使用说明(github Actions方式)</h4>
+
+![Fork位置与Settings位置](https://github.com/0honus0/1024-Autoreply/blob/master/doc/fork%20and%20settings.png)
+
+![Secrets位置](https://github.com/0honus0/1024-Autoreply/blob/master/doc/Secrets.png)
+
+![new secret位置](https://github.com/0honus0/1024-Autoreply/blob/master/doc/new%20Secret.png)
+<h4>1.Fork之后，通过settings -> Secrets -> new secret添加下列值</h4>
+
+(必需)
+
+        USER             用户名
+
+        PASSWORD         密码
+
+        SECRET           谷歌身份验证器密钥
+
+(可选1)([申请地址](https://market.aliyun.com/products/57124001/cmapi027426.html))
+
+        TOKER            阿里验证码识别接口AppCode
+
+(可选2)([注册地址](http://ttshitu.com/register.html?inviter=d14dbc3ccecc4df2b4e0eaebef556f13) )
+
+        CODEUSER         注册用户名
+
+        CODEPASS         注册密码
+
+ps:可选两个是识别验证码用的，任选其一即可，也可以都不选。如果不选碰见需要验证码的则会运行失败。
+
+       第一个用的是阿里一个api接口，每月30次免费的，正常是够用的
+
+       第二个是自己找的一个平台，1元可以识别500次，因为我最开始需要调试，所以用的这个
+
+代码中使用的就是第二个，如果想要使用第一个，只需修改`1024.py`文件中的`第八行`
+
+`from getver1 import Getver` 为 `from getver import Getver`
+
+<h4>2.(1)先点击Actions,同意使用协议。
+
+(2)然后随便修改任一文件的内容进行一次提交，就可以在Actions里面看见项目运行</h4>
+
+<h4>(可选)3.可以通过`getreply()中的reply与reply_m(0,n)(n设为回复的内容个数-1)`设置回复内容，`sleeptime设置为(1024,2048)之间`，可以根据需要修改。因为Actions一个项目限制6个小时，所以最大值不要超过2048。如果Actions显示运行超过六小时，先自行检查之前是否回复成功，若是，则把2048调整小一点。若不是，请提issues</h4>
+
+<h4>(可选)4.下面是自己下载py文件运行时的问题</h4>
+
+修改以下参数，记得用''括起来
+
+        user=''                 用户名
+
+        password=''             密码
+
+        secret=''               谷歌身份验证器密钥
+
+验证码部分根据自己选择修改参数类似上面
+
+有能力可以自己登陆修改为使用cookies登录，就不需要验证码部分了
+
+有bug反馈下，暂时没时间修，就放之后吧
+
+~~**后继可能添加功能：**(已实现)~~
+
+~~1.修改为github Actions的形式(已完成)~~
+
+~~2.添加邮件通知功能(失败github会自己发邮件)~~
+
+~~3.这种自动生成的api应该可以通过别的方式识别，再想办法(使用打码平台验证码识别接口)~~
